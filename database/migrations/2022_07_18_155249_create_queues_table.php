882c2d85458e0e537813b056473b3c59f34ddad7
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-07-18 16:07:13 +0700

    queue method
    make new table for queue method

---
+++ database/migrations/2022_07_18_155249_create_queues_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQueuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queues', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('queue');
+            $table->integer('distributor_id')->constrained('distributors');
+            $table->text('session');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('queues');
+    }
+}
