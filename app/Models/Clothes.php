906274ac4451bae68c1b1407e038705dd5843215
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-07-19 16:12:18 +0700

    new model
    not use model for now

---
+++ app/Models/Clothes.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Clothes extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function Type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function TemporaryStorage()
+    {
+        return $this->hasMany(TemporaryStorage::class);
+    }
+
+    public function TotalProduct()
+    {
+        return $this->hasMany(TotalProduct::class);
+    }
+
+    public function Image()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function BufferProduct()
+    {
+        return $this->hasMany(BufferProduct::class);
+    }
+
+    // public function Queue()
+    // {
+    //     return $this->hasOne(Queue::class);
+    // }
+}
