d67a0be789858c77a6a4a0011a4a5c0c2b915ccb
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-01-16 16:03:04 +0700

    add new controller for auth client

---
+++ app/Http/Controllers/Api/Client/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\Client;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Client\{ClientLoginRequest};
+
+class AuthController extends Controller
+{
+    public function login (ClientLoginRequest $request) {
+        try {
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
