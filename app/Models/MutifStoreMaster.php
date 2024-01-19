dd8460f5ad6af35d56d981ad3de7d0c1f240a91e
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-01-19 16:03:28 +0700

    move models into Models folder

--- app/Models/MutifStoreMaster.php
+++ app/Models/MutifStoreMaster.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MutifStoreMaster extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected static $logName = 'system';
+
+    protected $guarded = ['id'];
+
+    public function Distributor()
+    {
+        return $this->belongsTo(Distributor::class);
+    }
+
+    public function MutifStoreAddress()
+    {
+        return $this->hasMany(MutifStoreAddress::class);
+    }
 }
