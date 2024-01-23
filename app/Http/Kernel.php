25759879e3c703c26bd42bfe5f9694671785cd4a
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-01-23 18:41:17 +0700

    add new middleware for client zone

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
-        'Logger' => \App\Http\Middleware\Logger::class
+        'Logger' => \App\Http\Middleware\Logger::class,
+        'client-check' => \App\Http\Middleware\ClientCheckMIddleware::class
     ];
 }
