c0ea1f5ee78f28ac7af7361fbb811dfb9e453c4b
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-01-26 11:09:47 +0700

    add controlller for agent/ms/pm request

--- app/Http/Controllers/Api/Client/Event/ClientController.php
+++ app/Http/Controllers/Api/Client/Event/ClientController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api\Client\Event;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\{Distributor, PartnerAddress, PartnerGroup};
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\{Auth, DB};
-use App\Http\Requests\Client\Auth\ClientRegisterRequest;
+use App\Http\Requests\Client\Auth\{ClientRegisterRequest, DistributorRegisterRequest};
+use App\{Distributor, PartnerAddress, PartnerGroup, MutifStoreMaster, MutifStoreAddress};
 
 class ClientController extends Controller
 {
@@ -19,7 +20,7 @@ class ClientController extends Controller
                 $distributor = Distributor::create([
                     'name' => $request->name,
                     'phone' => $request->phone_1,
-                    'distributor_id' => ($request->partner_group_id == 1) ? 0 : $request->distributor_id,
+                    'distributor_id' => $request->distributor_id,
                     'partner_group_id' => $dataPartnerGroup->id,
                     'group_code' => $dataPartnerGroup->prtnr_code,
                     'level' => 'BRONZE',
@@ -27,7 +28,7 @@ class ClientController extends Controller
                 ]);
 
                 $this->createClientAddress($request, $distributor->id);
-
+                $this->createMutifStore($request, $distributor->id);
             DB::commit();
 
             return response()->json([
@@ -47,7 +48,9 @@ class ClientController extends Controller
     public function login(Request $request)
     {
         try {
-            $client = Distributor::select('id', 'phone')->where('phone', '=', $request->phone)->first();
+            $client = Distributor::select('distributors.id','partner_group_id','name','phone','pg.prtnr_name')
+                                ->join('partner_groups AS pg', 'pg.id', '=', 'distributors.partner_group_id')
+                                ->where('distributors.phone', '=', $request->phone)->first();
 
             if ($client == null) {
                 return response()->json([
@@ -74,11 +77,42 @@ class ClientController extends Controller
     public function partnerGroupList()
     {
         try {
-            $dataPartnerGroup = PartnerGroup::select('id', 'prtnr_name')->get();
+            $dataPartnerGroup = PartnerGroup::select('id', 'prtnr_name')->where('id', '<>', 1)->get();
 
             return response()->json([
                 'status' => 'success',
                 'data' => $dataPartnerGroup,
+                'error' => null
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'failed',
+                'data' => null,
+                'error' => $th->getMessage()
+            ], 400);
+        }
+    }
+
+    public function registerDistributor(DistributorRegisterRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+                $distributor = Distributor::create([
+                    'name' => $request->name,
+                    'phone' => $request->phone_1,
+                    'distributor_id' => $request->distributor_id,
+                    'partner_group_id' => 1,
+                    'group_code' => 'DB',
+                    'level' => 'BRONZE',
+                    'training_level' => 1
+                ]);
+
+                $this->createClientAddress($request, $distributor->id);
+            DB::commit();
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $distributor,
                 'error' => null
             ], 200);
         } catch (\Throwable $th) {
@@ -102,10 +136,46 @@ class ClientController extends Controller
         PartnerAddress::create([
             'distributor_id' => $client_id,
             'address' => $request->address,
-            'district' => '-',
-            'regency' => '-',
-            'province' => '-',
-            'phone_1' => $request->phone_1
+            'district' => $request->subdistrict,
+            'regency' => $request->city,
+            'province' => $request->province,
+            'phone_1' => $request->phone_1,
+            'zip' => $request->post_code,
+            'active' => true
         ]);
+    }
+
+    private function createMutifStore($request, $clientId)
+    {
+        $msCode = $this->createMsCode($request->open_date);
+
+        $mutifStoreMaster = MutifStoreMaster::create([
+            'mutif_store_name' => "$request->mutif_store_name $request->subdistrict",
+            'mutif_store_code' => $msCode,
+            'distributor_id' => $clientId,
+            'open_date' => $request->open_date,
+            'status' => 'active'
+        ]);
+
+        MutifStoreAddress::create([
+            'mutif_store_master_id' => $mutifStoreMaster->id,
+            'address' => $request->address,
+            'province' => $request->province,
+            'regency' => $request->city,
+            'district' => $request->subdistrict,
+            'phone_1' => $request->phone_1,
+            'addr_type' => 'shipTo',
+            'zip' => $request->zip,
+        ]);
+    }
+
+    private function createMsCode($openDate)
+    {
+        $randomInteger = rand(0000, 9999);
+        $parseOpenDate = Carbon::parse($openDate)->format('d/m/y');
+
+        $msCode = "$randomInteger/MS/$parseOpenDate";
+
+        return $msCode;
     }
 }
