cf2f49ec5b7e71ab863752672b583c5254e6fe82
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-02-13 15:51:27 +0700

    add new report API

--- app/Http/Controllers/Api/Admin/Event/ReportController.php
+++ app/Http/Controllers/Api/Admin/Event/ReportController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Admin\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Models\{Order, Distributor, Entity};
+use App\Models\{Order, Distributor, Entity, Product};
 
 class ReportController extends Controller
 {
@@ -165,6 +165,113 @@ class ReportController extends Controller
             return response()->json([
                 'status' => 'success',
                 'data' => $dataOrderProduct,
+                'error' => null
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'failed',
+                'data' => null,
+                'error' => $th->getMessage()
+            ], 400);
+        }
+    }
+
+    public function ReportPerProduct($id)
+    {
+        try {
+            $product = Product::select(
+                'products.id',
+                'products.entity_name',
+                'products.article_name',
+                DB::raw('SUM(size_S + size_M + size_L + size_XL + size_XXL + size_XXXL + size_2 + size_4 + size_6 + size_8 + size_10 + size_12 + size_27 + size_28 + size_29 + size_30 + size_31 + size_32 + size_33 + size_34 + size_35 + size_36 + size_37 + size_38 + size_39 + size_40 + size_41 + size_42 + size_other) AS total_order'),
+            )->leftJoin('orders', 'orders.product_id', '=', 'products.id')
+            ->where('products.id', '=', $id)
+            ->with(['DetailOrder' => function ($query) {
+                $query->select(
+                        'orders.client_id',
+                        'orders.product_id',
+                        DB::raw('distributors.name AS agent_name'),
+                        DB::raw('db.name AS distributor_name'),
+                        DB::raw('events.event_name'),
+                        DB::raw('SUM(size_S + size_M + size_L + size_XL + size_XXL + size_XXXL + size_2 + size_4 + size_6 + size_8 + size_10 + size_12 + size_27 + size_28 + size_29 + size_30 + size_31 + size_32 + size_33 + size_34 + size_35 + size_36 + size_37 + size_38 + size_39 + size_40 + size_41 + size_42 + size_other) AS total_order'),
+                        'orders.size_S',
+                        'orders.size_M',
+                        'orders.size_L',
+                        'orders.size_XL',
+                        'orders.size_XXL',
+                        'orders.size_XXXL',
+                        'orders.size_2',
+                        'orders.size_4',
+                        'orders.size_6',
+                        'orders.size_8',
+                        'orders.size_10',
+                        'orders.size_12',
+                        'orders.size_27',
+                        'orders.size_28',
+                        'orders.size_29',
+                        'orders.size_30',
+                        'orders.size_31',
+                        'orders.size_32',
+                        'orders.size_33',
+                        'orders.size_34',
+                        'orders.size_35',
+                        'orders.size_36',
+                        'orders.size_37',
+                        'orders.size_38',
+                        'orders.size_39',
+                        'orders.size_40',
+                        'orders.size_41',
+                        'orders.size_42',
+                        'orders.size_other',
+                    )
+                    ->leftJoin('distributors', 'distributors.id', '=', 'orders.client_id')
+                    ->leftJoin('distributors AS db', 'db.id', '=', 'distributors.distributor_id')
+                    ->leftJoin('events', 'events.id', '=', 'orders.event_id')
+                    ->groupBy(
+                        'orders.client_id',
+                        'orders.product_id',
+                        'distributors.name',
+                        'db.name',
+                        'events.event_name',
+
+                        'orders.size_S',
+                        'orders.size_M',
+                        'orders.size_L',
+                        'orders.size_XL',
+                        'orders.size_XXL',
+                        'orders.size_XXXL',
+                        'orders.size_2',
+                        'orders.size_4',
+                        'orders.size_6',
+                        'orders.size_8',
+                        'orders.size_10',
+                        'orders.size_12',
+                        'orders.size_27',
+                        'orders.size_28',
+                        'orders.size_29',
+                        'orders.size_30',
+                        'orders.size_31',
+                        'orders.size_32',
+                        'orders.size_33',
+                        'orders.size_34',
+                        'orders.size_35',
+                        'orders.size_36',
+                        'orders.size_37',
+                        'orders.size_38',
+                        'orders.size_39',
+                        'orders.size_40',
+                        'orders.size_41',
+                        'orders.size_42',
+                        'orders.size_other'
+                    )
+                    ;
+            }])
+            ->groupBy('products.id','products.entity_name','products.article_name')
+            ->first();
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $product,
                 'error' => null
             ], 200);
         } catch (\Throwable $th) {
