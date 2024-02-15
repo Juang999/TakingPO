97e5858ef73f76f42902c5cf7eba118962822417
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-02-15 15:11:48 +0700

    update model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,9 @@ class Product extends Model
     {
         return $this->hasMany(DetailSession::class, 'product_id', 'id');
     }
+
+    public function DetailOrder()
+    {
+        return $this->hasMany(Order::class, 'product_id', 'id');
+    }
 }
