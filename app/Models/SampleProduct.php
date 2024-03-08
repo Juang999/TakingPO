3c5ed12a54ab8bbde8880f598da1729eddb5a9f8
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-03-08 08:50:31 +0700

    update models for sample model

--- app/Models/SampleProduct.php
+++ app/Models/SampleProduct.php
@@ -3,13 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class SampleProduct extends Model
 {
+    use LogsActivity;
+
     protected $guarded = ['id'];
+
+    protected static $logUnguarded = true;
+
+    protected static $logName = 'system';
 
     public function PhotoSampleProduct()
     {
         return $this->hasMany(SampleProductPhoto::class, 'sample_product_id', 'id');
+    }
+
+    public function SinglePhotoProduct()
+    {
+        return $this->hasOne(SampleProductPhoto::class, 'sample_product_id', 'id');
     }
 }
--- app/Models/SampleProductPhoto.php
+++ app/Models/SampleProductPhoto.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class SampleProductPhoto extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity;
 
     protected $guarded = ['id'];
+
+    protected static $logUnguarded = true;
+
+    protected static $logName = 'system';
 
     public function SampleProduct()
     {
