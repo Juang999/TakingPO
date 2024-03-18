5737b2055d068674bc05ec5671ff61d27dd1ccb6
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-03-18 08:15:14 +0700

    add logactivity in model

--- app/Models/FabricTexture.php
+++ app/Models/FabricTexture.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class FabricTexture extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity;
+
+    protected static $logUnguarded = true;
+
+    protected static $logName = 'system';
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 }
