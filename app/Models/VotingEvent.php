821ca87140a8685b3967d9864b2f9d9a3becc6a6
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-03-18 15:02:52 +0700

    add new relationships for several model

--- app/Models/VotingEvent.php
+++ app/Models/VotingEvent.php
@@ -14,4 +14,14 @@ class VotingEvent extends Model
     protected static $logUnguarded = true;
 
     protected static $logName = 'system';
+
+    public function Member()
+    {
+        return $this->hasMany(VotingMember::class, 'voting_event_id', 'id');
+    }
+
+    public function Sample()
+    {
+        return $this->hasMany(VotingSample::class, 'voting_event_id', 'id');
+    }
 }
