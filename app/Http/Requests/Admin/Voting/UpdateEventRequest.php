a78f3572f15fc0255f3d158293428da4e3a16944
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-03-20 10:02:13 +0700

    add new request file

---
+++ app/Http/Requests/Admin/Voting/UpdateEventRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin\Voting;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'start_date' => 'nullable|date',
+            'title' => 'nullable|string',
+            'description' => 'nullable|string',
+        ];
+    }
+}
