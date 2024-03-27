30adcfdb2ffc318f836bde9a9eb60b6554bbe7c5
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-03-27 10:22:22 +0700

    update controller

--- app/Http/Controllers/Api/Admin/ResourceAndDevelopment/SampleProductController.php
+++ app/Http/Controllers/Api/Admin/ResourceAndDevelopment/SampleProductController.php
@@ -190,7 +190,7 @@ class SampleProductController extends Controller
                     'size' => $requests['size'],
                     'accessories' => $requests['accessories'],
                     'note_and_description' => $requests['note_and_description'],
-                    'file_design' => $requests['file_design'],
+                    'design_file' => $requests['design_file'],
                     'designer_id' => $requests['designer_id'],
                     'md_id' => $requests['md_id'],
                     'leader_designer_id' => $requests['leader_designer_id'],
@@ -514,7 +514,7 @@ class SampleProductController extends Controller
 
     private function requestUpdateSampelProduct($request, $id)
     {
-        $sampleProduct = SampleProduct::select('id','date','article_name', 'style_id', 'entity_name', 'material', 'size', 'accessories', 'note_and_description', 'file_design', 'designer_id', 'md_id', 'leader_designer_id')
+        $sampleProduct = SampleProduct::select('id','date','article_name', 'style_id', 'entity_name', 'material', 'size', 'accessories', 'note_and_description', 'design_file', 'designer_id', 'md_id', 'leader_designer_id')
                                     ->where('id', '=', $id)
                                     ->first();
 
@@ -531,7 +531,7 @@ class SampleProductController extends Controller
             'size' => ($request->size) ? $request->size : $sampleProduct->size,
             'accessories' => ($request->accessories) ? $request->accessories : $sampleProduct->accessories,
             'note_and_description' => ($request->note_description) ? $request->note_description : $sampleProduct->note_and_description,
-            'file_design' => ($request->file_design) ? $request->file_design : $sampleProduct->file_design,
+            'design_file' => ($request->design_file) ? $request->design_file : $sampleProduct->design_file,
             'designer_id' => $designerId,
             'md_id' => $merchandiserId,
             'leader_designer_id' => $leaderDesignerId,
